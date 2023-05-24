848a8225a5f62d7c515b8bbc6ab602c8dc7dedc4
Revi1337 <david122123@gmail.com>
2023-05-24 19:12:12 +0900

    @Conditional 다양한 기능
    @ConditionalOnClass , @ConditionalOnMissingClass
        --> 클래스가 있는 경우 동작한다. 나머지는 그 반대
    @ConditionalOnBean , @ConditionalOnMissingBean
        --> 빈이 등록되어 있는 경우 동작한다. 나머지는 그 반대
    @ConditionalOnProperty
        --> 환경 정보가 있는 경우 동작한다.
    @ConditionalOnResource
        --> 리소스가 있는 경우 동작한다.
    @ConditionalOnWebApplication , @ConditionalOnNotWebApplication
        --> 웹 애플리케이션인 경우 동작한다.
    @ConditionalOnExpression
        --> SpEL 표현식에 만족하는 경우 동작한다

--- autoconfig/src/main/java/com/example/autoconfig/config/MemoryConfig.java
+++ autoconfig/src/main/java/com/example/autoconfig/config/MemoryConfig.java
@@ -2,12 +2,14 @@ package com.example.autoconfig.config;
 
 import com.example.memory.MemoryController;
 import com.example.memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class) // -Dmemory=on 를 사용했을떄만 실행되도록 구성
+//@Conditional(MemoryCondition.class) // -Dmemory=on 를 사용했을떄만 실행되도록 구성
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 환경정보가 memory=on 이라는 조건에 맞으면 동작하고 그렇지 않으면 동작하지 않음.
 public class MemoryConfig {
 
     @Bean
