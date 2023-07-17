1bdfa77cc8d7bedcab1f52a46041bbdbc2105fa6
Revi1337 <david122123@gmail.com>
2023-07-17 13:41:06 +0900

    [외부설정과 프로필 2 - Environment]

--- external-read/src/main/java/hello/externalread/ExternalReadApplication.java
+++ external-read/src/main/java/hello/externalread/ExternalReadApplication.java
@@ -1,9 +1,13 @@
 package hello.externalread;
 
+import hello.externalread.config.MyDatasourceEnvConfig;
+import hello.externalread.datasource.MyDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDatasourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.externalread.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
---
+++ external-read/src/main/java/hello/externalread/config/MyDatasourceEnvConfig.java
@@ -0,0 +1,29 @@
+package hello.externalread.config;
+
+import hello.externalread.datasource.MyDataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j @Configuration @RequiredArgsConstructor
+public class MyDatasourceEnvConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public MyDataSource myDataSource() {
+        String url = environment.getProperty("my.datasource.url");
+        String username = environment.getProperty("my.datasource.username");
+        String password = environment.getProperty("my.datasource.password");
+        int maxConnection = environment.getProperty("my.datasource.etc.max-connection", Integer.class);
+        Duration timeout = environment.getProperty("my.datasource.etc.timeout", Duration.class);
+        List<String> options = environment.getProperty("my.datasource.etc.options", List.class);
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+
+}
